385c877d2ca938b1e2d0e648bc39e9865f873bb6
b-delacruz <brianmdelacruz@gmail.com>
2022-06-25 15:34:35 -0400

    make oauth work

---
+++ controllers/profiles.js
@@ -0,0 +1,12 @@
+
+
+function index(req, res) {
+    res.render('profiles/index', {
+      profiles, // same as profiles: profiles
+      name: req.query.name,
+      user: req.user
+    })
+  }
+export {
+
+}

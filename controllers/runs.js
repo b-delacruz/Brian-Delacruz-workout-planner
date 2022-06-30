b15747528f32298d9ca8c02475b1bf02fc0017f2
b-delacruz <brianmdelacruz@gmail.com>
2022-06-30 09:49:57 -0400

    add styling to runs pg.

--- controllers/runs.js
+++ controllers/runs.js
@@ -26,7 +26,7 @@ function create (req, res) {
   req.body.owner = req.user.profile._id
   Run.create(req.body)
   .then(run => {
-    res.redirect('/runs')
+    res.redirect('/runs',{newRunDate})
   })
   .catch(error => {
     console.log(error)
